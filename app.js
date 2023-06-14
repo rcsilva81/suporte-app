2483d2bac4a459a5bf32d938fff037c748ea7938
Robert Silva <68916995+rcsilva81@users.noreply.github.com>
2023-06-14 16:09:39 -0300

    change4

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+import {routes} from './routes.js'
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+
 
 var app = express();
+
+export const routes = express.Router();
+
+routes.get("/", './routes/index');
+routes.get("/users", "./routes/users");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +27,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+app.use(routes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
